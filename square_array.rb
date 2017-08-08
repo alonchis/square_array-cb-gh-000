0e3877489fd5544494b8268ae09b26962d8121f6
alonso arteaga <z1791049@students.niu.edu>
2017-08-08 03:50:35 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+
+  #working code easy
+  # new_array = []
+  # array.each {|index|
+  #
+  #   new_array << index**2
+  #
+  # }
+  # return new_array
+
+  #trying Advanced
+  ary.new
+
+  ary = array.collect**2
+  return ary
 end
